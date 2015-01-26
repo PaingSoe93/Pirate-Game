281013369be27eab6dbd3e56ffd8ef6d3b3e7f92
Paing Soe <paingsoeaung.93@gmail.com>
2015-01-26 22:20:33 +0630

    Initial Commit

---
+++ Pirate Adv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adv
+//
+//  Created by Paing Soe on 1/26/15.
+//  Copyright (c) 2015 Pai Innovation Co.,ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pirate Adv/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pirate Adv
+//
+//  Created by Paing Soe on 1/26/15.
+//  Copyright (c) 2015 Pai Innovation Co.,ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
